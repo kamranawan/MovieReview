5d6268ee7887ad25841aee49ae3afe55562bdc1e
Kamran awan <kamranawan1@yahoo.com>
2018-08-02 14:37:52 +0500

    changes made

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
-	belongs_to :user
+	validates :name,  presence: true
+  	validates :year, presence: true
+  	belongs_to :user
 	has_many :reviews
 	has_attached_file :image, styles: { medium: "400x400>", thumb: "300x200>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
+	# validates :Rate, length: { in: 1..5 }
+  	validates :comment, presence: true
+
 	belongs_to :movie
 	belongs_to :user
 end
