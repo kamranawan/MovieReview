f04efbb0471471ca154f855ecd0ea2ed0b142d2c
Kamran awan <kamranawan1@yahoo.com>
2018-08-06 19:37:25 +0500

    update rating model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,11 @@
 class Movie < ApplicationRecord
 	validates :name,  presence: true
-  	validates :year, presence: true
+  	# validates :year, presence: true
+  	validates :rating, presence: true
+  	validates :year, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  	# validates :rating, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  	# validates_inclusion_of :rating, message: "Only Rate 1 to 5 without sign"
+
   	belongs_to :user
 	has_many :reviews
 	has_attached_file :image, styles: { medium: "400x400>", thumb: "300x200>" }, default_url: "/images/:style/missing.png"
